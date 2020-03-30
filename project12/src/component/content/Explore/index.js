2e562f917bd943fc267db4a8f4c216a73c5f864d
artupAde <ade.lesmana@bni.co.id>
2020-03-30 21:45:13 +0700

    upt

--- project12/src/component/content/Explore/index.js
+++ project12/src/component/content/Explore/index.js
@@ -3,59 +3,56 @@ import Header from "../../header";
 import CoHead from "../../content/Contents/ContentHeader";
 import { connect, useDispatch, useSelector } from "react-redux";
 import {
-  fetchSwapiAction,
-  updateUrlDetailAction,
-  clearSwapiAction,
-  fetchProfileData
+  fetchSwapiAction
 } from "../../../redux/action";
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Explorer extends React.Component {
-
   componentDidMount() {
     this.props.fetchSwapiAction();
   }
 
   render() {
-    const { loadingProfile, selectedProfile } = this.props
+    const { loadingProfile, selectedProfile } = this.props;
     return (
       <React.Fragment>
         <Header />
         <CoHead />
-        <div
-          style={{
-            width: "100%",
-            marginTop: "10vh",
-            borderTop: "1px solid #222"
-          }}
-        >
-          <p>
-            {
-              this.props.swapiD.map(data => {
-              return (
+
+        <div className="row">
+        {this.props.swapiD.map(data => {
+          return (
+            
+              <div className="col-sm-4">
                 <div className="card-content-fth ">
                   <div className="card pmee sec">
                     <div className="card-body">
-                      <h5 className="card-title">{data['Title']}</h5>
+                      <h5 className="card-title">{data["Title"]}</h5>
                       <hr />
                       {/* <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
                       <p className="card-text">
-                        
-                        <img src={data['Poster']} style={{width:'200px', height:'200px'}}/>
+                        <img
+                          src={data["Poster"]}
+                          style={{ width: "200px", height: "200px" }}
+                        />
                       </p>
                       <label>0 views</label>
                       <br />
-                      <Link to="/Explore" className="btn btn-info btn-sm card-photo-btn">
+                      <Link
+                        to="/Detail"
+                        className="btn btn-info btn-sm card-photo-btn"
+                        onClick=""
+                      >
                         View Detail
                       </Link>
                     </div>
                   </div>
                 </div>
-              )
-             })
-            }
-          </p>
+              </div>
+            
+          );
+        })}
         </div>
       </React.Fragment>
     );
@@ -63,16 +60,10 @@ class Explorer extends React.Component {
 }
 
 const mapStateToProps = state => ({
-  swapiD: state.swapiD.data,
-  // selectedProfile: state.selectedProfile.data,
-  // loadingProfile: state.selectedProfile.loading,
-  // urlDetail: state.selectedProfile.urlDetail
+  swapiD: state.swapiD.data
 });
 const mapDispatchToProps = dispatch => ({
-  fetchSwapiAction: () => dispatch(fetchSwapiAction()),
-  // updateUrlDetailAction: () => dispatch(updateUrlDetailAction()),
-  // clearSwapiAction: () => dispatch(clearSwapiAction()),
-  // fetchProfileData: () => dispatch(fetchProfileData())
+  fetchSwapiAction: () => dispatch(fetchSwapiAction())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Explorer);
